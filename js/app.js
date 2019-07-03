82f3330bb83ddf3ca40d392e39f9d5d369667263
Joe Domovitch <jdomovitch@gmail.com>
2019-07-03 14:58:58 -0700

    add Adventure Time styling

--- js/app.js
+++ js/app.js
@@ -1,68 +1,85 @@
+let newToma;
+
 class Tomagotchi {
   constructor(name) {
     this.name = name;
     this.age = 0;
-    this.hunger = 1;
-    this.fatigue = 1;
-    this.boredom = 1;
+    this.hungry = 1;
+    this.tired = 1;
+    this.bored = 1;
     this.time = 0;
-  };
+  }
   initTomagotchi() {
     const $tamDiv = $('<div id="tomagotchi">');
-    const $img = $('<img src="http://images3.wikia.nocookie.net/__cb20070519201025/tamagotchi/images/3/32/Marutchi.png">');
+    const $img = $('<img src="https://i.ibb.co/c1GfyH2/5c80f67c72f5d9028c17ed1c.png" alt="5c80f67c72f5d9028c17ed1c" border="0">');
     $tamDiv.append($img);
 
     const $actions = $('<div id="actions-div">');
-    const $feed = $('<button class="actions">Feed</button>');
-    const $lights = $('<button class="actions">Lights Out</button>');
-    const $play = $('<button class="actions">Play</button>');
+    const $feed = $('<button class="actions" id="feed">Feed</button>');
+    const $lights = $('<button class="actions" id="lights">Lights Out</button>');
+    const $play = $('<button class="actions" id="play">Play</button>');
+
+    
     $actions.append($feed);
     $actions.append($lights);
     $actions.append($play);
-    $('.container').append($actions);
     
+    
+    $('.container').append($actions);
     $('.container').append($tamDiv);
-
-
-
+    
     $('#name').html(`Name: <span>${this.name}</span>`);
     $('#age').text(`Age: ${this.age}`);
-    $('#hunger').text(`Hunger: ${this.hunger}`);
-    $('#fatigue').text(`Fatigue: ${this.fatigue}`);
-    $('#boredom').text(`Boredom: ${this.boredom}`);
-  };
-  setTimer() {
+    $('#hunger').text(`Hunger: ${this.hungry}`);
+    $('#fatigue').text(`Fatigue: ${this.tired}`);
+    $('#boredom').text(`Boredom: ${this.bored}`);
+    
+    $('#lights').on('click', (e) => {
+      console.log(e.target); 
+    });
+  }
+  statIntervals() {
     setInterval(() => {
       this.time += 1;
       console.log(this.time);
     if (!(this.time % 30)) {
-      this.boredom += 1;
-      $('#boredom').text(`Boredom: ${this.boredom}`);
+      this.bored += 1;
+      $('#boredom').text(`Boredom: ${this.bored}`);
       // console.log(this.boredom);
     }
     if (!(this.time % 45)) {
-      this.hunger += 1;
-      $('#hunger').text(`Hunger: ${this.hunger}`);
+      this.hungry += 1;
+      $('#hunger').text(`Hunger: ${this.hungry}`);
     }
     if (!(this.time % 100)) {
-      this.boredom += 1;
-      $('#fatigue').text(`Fatigue: ${this.fatigue}`);
+      this.tired += 1;
+      $('#fatigue').text(`Fatigue: ${this.tired}`);
     }
-    if (!(this.time % 150)) {
+    if (!(this.time % 180)) {
       this.age += 1;
       $('#age').text(`Age: ${this.age}`);
     }
-    }, 1000)
-  }
 
+    this.checkLevel(this);
+    }, 1000);
+  }
+  checkLevel() {
+    for (let prop in this) {
+      if (this[prop] === 10 && (prop !== 'time')) {
+        alert(`${this.name} died, they were too ${prop}.`);
+      }
+    }  
+  }
 };
 
-$('button').on('click', (e) => {
+//put below in game object
+
+$('#spawn').on('click', (e) => {
   $(e.target).hide();
 
   const tomaName = prompt("Give your pet a funky name.");
-  const newToma = new Tomagotchi(tomaName);
+  newToma = new Tomagotchi(tomaName);
 
   newToma.initTomagotchi();
-  newToma.setTimer();
+  newToma.statIntervals();
 });
